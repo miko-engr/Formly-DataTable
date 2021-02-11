c4ee7d221e21438d4bd5e408d8ba32ae31e34dee
Miko Lasso <lassomikoengr@gmail.com>
2021-02-11 03:57:53 -0500

    added ag-grid-community

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,47 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormlyModule } from '@ngx-formly/core/';
+import { FormlyBootstrapModule } from '@ngx-formly/bootstrap/';
+import { AgGridModule } from 'ag-grid-angular';
 
 import { AppComponent } from './app.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { FormlyModule } from '@ngx-formly/core';
-import { FormlyBootstrapModule } from '@ngx-formly/bootstrap';
-
-import { NgxDatatableModule } from '@swimlane/ngx-datatable';
-
+import { GridTypeComponent } from './grid.type';
+import { GridFormlyCellComponent } from './grid-formly-cell.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    NgxDatatableModule,
     BrowserModule,
     ReactiveFormsModule,
-    FormlyModule.forRoot({ extras: { lazyRender: true } }),
-    FormlyBootstrapModule
+    FormlyBootstrapModule,
+    AgGridModule.withComponents([GridFormlyCellComponent]),
+    FormlyBootstrapModule,
+    FormlyBootstrapModule,
+    FormlyModule.forRoot({
+      types: [
+        {
+          name: 'grid',
+          component: GridTypeComponent,
+          defaultOptions: {
+            templateOptions: {
+              width: '100%',
+              height: '400px',
+            },
+          },
+        },
+      ],
+    }),
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  declarations: [
+    AppComponent,
+    GridTypeComponent,
+    GridFormlyCellComponent,
+  ],
 })
 export class AppModule { }
+
+
+/**  Copyright 2018 Google Inc. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at http://angular.io/license */
